f47ad9c06e9b5747885cdda7154e1f1be20445e2
miko-github <mikoloism.github@gmail.com>
2021-04-04 17:35:56 +0430

    Update Store Actions,

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import TodoForm from '../views/TodoForm.vue'
+import TodoHome from '../views/TodoHome.vue'
+import TodoFormNew from '../views/Forms/TodoNew.vue'
+import TodoList from '../layouts/todo-item-group.vue'
 import About from '../views/About.vue'
 
 Vue.use(VueRouter)
@@ -14,8 +16,25 @@ const routes = [
   },
   {
     path: '/todo',
-    name: 'TodoForm',
-    component: TodoForm,
+    component: TodoHome,
+    children: [
+      { path: '', name: 'TodoList', component: TodoList },
+      {
+        path: '/todo/new',
+        name: 'TodoFormNew',
+        component: TodoFormNew,
+      },
+      {
+        path: '/todo/update/:id',
+        name: 'WTFUpdate',
+        component: TodoFormNew,
+      },
+      {
+        path: '/todo/read/:id',
+        name: 'WTFRead',
+        component: TodoFormNew,
+      },
+    ],
   },
   {
     path: '/about',
--- src/store/index.js
+++ src/store/index.js
@@ -2,41 +2,137 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import _ from 'lodash'
 import storage from './../plugins/local-storage'
-
+import Schems, { Types } from './../plugins/schems'
+const { STRING, BOOLEAN } = Types
 Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
-    todoLastInserted: undefined,
-    todoLastInsertId: undefined,
-    todoItems: [{ id: 0, title: 'Hi' }],
+    TodoLastUpdated: undefined,
+    TodoLastInserted: undefined,
+    TodoItems: [],
+    TodoSchema: Schems({
+      id: {
+        type: STRING,
+      },
+      title: {
+        type: STRING,
+        def: 'Simple Title Todo',
+      },
+      description: {
+        type: STRING,
+        default: '',
+      },
+      isDone: {
+        type: BOOLEAN,
+        default: false,
+      },
+    }),
+    TodoSyncing: false,
+    TodoSynced: [false, undefined],
   },
-  actions: {},
+  actions: {
+    TODO_VALIDATION: ({ state }, prop) => state.TodoSchema.validate(prop),
+    TODO_READ: ({ state }, index) => {
+      const id = _.isLength(index) ? `todo-item--${index}` : index
+      if (index) return _.filter(state.TodoItems, (item) => item.id === id)
+      return state.TodoItems
+    },
+    TODO_INSERT: async ({ dispatch, commit }, todo) => {
+      if (todo.title) {
+        return dispatch('TODO_VALIDATION', {
+          ...todo,
+          id: _.uniqueId('todo-item--'),
+        })
+          .then(({ res }) => {
+            console.log(res)
+            commit('TODO_INSERT', res)
+            return true
+          })
+          .catch((err) => {
+            throw err
+          })
+      } else return false
+    },
+    TODO_DELETE: async ({ dispatch, commit }, index) => {
+      return dispatch('TODO_READ', index)
+        .then((todo) => {
+          commit('TODO_DELETE', todo)
+          return true
+        })
+        .catch((err) => {
+          throw err
+        })
+    },
+    TODO_UPDATE: ({ dispatch }, index, updated) => {
+      return dispatch('TODO_READ', index)
+        .then((todoObject) => {
+          const newTodo = { ...todoObject, ...updated }
+          return dispatch('TODO_VALIDATION', newTodo)
+            .then(({ res }) => {
+              return dispatch('TODO_DELETE', res.id)
+                .then(() => {
+                  return dispatch('TODO_INSERT', res)
+                    .then(() => res)
+                    .catch(() => false)
+                })
+                .catch((err) => {
+                  throw err
+                })
+            })
+            .catch((err) => {
+              throw err
+            })
+        })
+        .catch((err) => {
+          throw err
+        })
+    },
+    TODO_CHANGE: ({ commit, dispatch }, index, changed) => {
+      dispatch('TODO_READ', index)
+        .then((item) => commit('TODO_CHANGE', item, changed))
+        .catch((err) => {
+          throw err
+        })
+    },
+
+    TODO_SYNC: async ({ commit, dispatch }) => {
+      return dispatch('TODO_READ')
+        .then((items) => {
+          commit('TODO_SYNCED')
+          return storage().sync(items)
+        })
+        .catch((err) => {
+          throw err
+        })
+    },
+    TODO_READ_SYNC: () => storage().parse(),
+    TODO_LAST_INSERT_SYNC: ({ state }) =>
+      storage().push(state.TodoLastInserted),
+    TODO_LAST_UPDATE_SYNC: ({ state }) => storage().push(state.TodoLastUpdated),
+  },
   mutations: {
-    alt: () => window.alert('hi'),
-    todoUpdate: (state, id, newItem) => ({ state, id, newItem }),
-    todoRemove: (state, id) => {
-      const selectedItem = _.filter(state.todo.items, { id })
-      const removedItem = _.remove(state.todo.items, (item) =>
-        _.isEqual(item, selectedItem)
-      )
-      return removedItem
+    TODO_INSERT: (state, todo) => {
+      state.TodoItems.push(todo)
+      state.TodoLastInserted = todo
     },
-    todoInsert: (state, item) => {
-      const id = _.uniqueId('todo-item-')
-      const todoItem = { id, ...item }
-      state.todoItems.push({ ...todoItem })
-      state.todoLastInsertId = id
-      state.todoLastInserted = todoItem
-      return id
+    TODO_DELETE: (state, todoObject) => {
+      if (todoObject)
+        _.remove(state.TodoItems, (item) => item.id === todoObject.id)
     },
-    todoGet: (state, id) =>
-      id ? _.filter(state, (item) => _.isEqual(item.id, id)) : state.todoItems,
-    todoSync: (state) => {
-      const { todoItems } = state
-      return storage().sync(todoItems)
+    TODO_TOGGLE_CHECK: ({ TodoItems }, index) => {
+      const selectedItem = _.filter(TodoItems, (item) => item.id === index)
+      if (selectedItem.length > 0)
+        selectedItem[0].isDone = !selectedItem[0].isDone
+      console.log(selectedItem)
     },
-    todoDownload: () => storage().parse(),
-    todoUpload: (state) => storage().push(state.todoLastInserted),
+    TODO_CHANGE: ({ TodoItems }, prevItem, nextItem) => {
+      const value = { ...prevItem, ...nextItem }
+      const selected = _.filter(TodoItems, (item) => item.id === prevItem.id)
+      if (selected.length > 0) selected[0] = { ...value }
+    },
+    TODO_SYNCED: (state) => {
+      state.TodoSynced = [true, new Date()]
+    },
   },
   modules: {},
 })
